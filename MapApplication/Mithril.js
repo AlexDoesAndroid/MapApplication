3dfa267770815ccd23fc6cd19e1ef3a89ae406fa
Kyle White <tuh32402@temple.edu>
2021-11-17 21:04:35 -0500

    Created style sheets and migrated javascript to Mithril.js, if your reading this go to mithrils website and follow the hello world tutorial. KW

--- MapApplication/Mithril.js
+++ MapApplication/Mithril.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+    var $winwidth = $(window).width();
+    $(".bg").attr({
+        width: $winwidth
+    });
+    $(window).bind("resize", function () {
+        var $winwidth = $(window).width();
+        $("bg").attr({
+            width: $winwidth
+        });
+    });
+});
