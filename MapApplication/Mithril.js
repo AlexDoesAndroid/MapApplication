79fe66e6b9d82d297e55c505a0403a2fab5d0c58
doingthisforclass <zam0400@gmail.com>
2021-11-19 13:54:48 -0500

    modal test

--- MapApplication/Mithril.js
+++ MapApplication/Mithril.js
@@ -21,3 +21,25 @@ var App = {
 }
 m.mount(document.body, App);
 //*/
+
+
+
+//modal module
+var modal = {}
+modal.visible = m.prop(false)
+modal.view = function (body) {
+    return modal.visible() ? m(".modal", body()) : ""
+}
+
+//in your other view
+var myOtherView = function () {
+    //this button sets the flag to true
+    m("button[type=button]", { onclick: modal.visible.bind(this, true) }, "Show modal"),
+
+        //include the modal anywhere it makes sense to
+        //its visibility is taken care by the modal itself
+        //positioning is controlled via CSS
+        modal.view(function () {
+            return m("p, "modal content goes here")
+  })
+}
