8d7828b517482d4a030ff3196afd446f5e73a291
doingthisforclass <zam0400@gmail.com>
2021-11-21 13:18:42 -0500

    text

--- MapApplication/Mithril.js
+++ MapApplication/Mithril.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 //comment the below out to get the map back
 ///*
 var app = document.getElementById("hello")
-m.render(app, "Hello world");
+m.render(app, "Hello world"); 
 //*/
 
 
@@ -27,18 +27,21 @@ m.render(app, "Hello world");
 var modal = {}
 modal.visible = false;
 modal.view = function (body) {
-    return modal.visible() ? m(".modal", body()) : ""
+    return modal.visible() ? m.render(".modal", body()) : ""
 }
 
 //in your other view
 var myOtherView = function () {
     //this button sets the flag to true
-    m("button[type=button]", { onclick: modal.visible.bind(this, true) }, "Show modal"),
+    m.render("button[type=button]", { onclick: modal.visible.bind(this, true) }, "Show modal"),
 
         //include the modal anywhere it makes sense to
         //its visibility is taken care by the modal itself
         //positioning is controlled via CSS
         modal.view(function () {
-            return m("p", "modal content goes here")
+            return m.render("p", "modal content goes here")
   })
 }
+
+
+m.mount(document.body, Component)
