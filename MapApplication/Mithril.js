00b46d15dd59ab5bc4e71add2741f735edfbef5c
doingthisforclass <zam0400@gmail.com>
2021-11-22 10:35:01 -0500

    sync

--- MapApplication/Mithril.js
+++ MapApplication/Mithril.js
@@ -1,21 +1,21 @@
 ﻿
-$(document).ready(function () {
-    var $winwidth = $(window).width();
-    $(".bg").attr({
-        width: $winwidth
-    });
-    $(window).bind("resize", function () {
-        var $winwidth = $(window).width();
-        $("bg").attr({
-            width: $winwidth
-        });
-    });
-});
+//$(document).ready(function () {
+//    var $winwidth = $(window).width();
+//    $(".bg").attr({
+//        width: $winwidth
+//    });
+//    $(window).bind("resize", function () {
+//        var $winwidth = $(window).width();
+//        $("bg").attr({
+//            width: $winwidth
+//        });
+//    }); 
+//});
 
 //comment the below out to get the map back
 ///*
-var app = document.getElementById("hello")
-m.render(app, "Hello world"); 
+
+
 //*/
 
 
@@ -26,18 +26,15 @@ m.render(app, "Hello world");
 
 /*
 var modal = {
-
 }
 modal.visible = false;
 modal.view = function (body) {
     return modal.visible() ? m.render(".modal", body()) : ""
 }
-
 //in your other view
 var myOtherView = function () {
     //this button sets the flag to true
     m('button[type="button"]', { onclick: modal.visible.bind(this, true) }, "Show modal"),
-
         //include the modal anywhere it makes sense to
         //its visibility is taken care by the modal itself
         //positioning is controlled via CSS
@@ -45,10 +42,16 @@ var myOtherView = function () {
             return m.render("p", "modal content goes here")
   })
 }
-
 document.getElementById("#modal").onclick = m.withAttr(myOtherView());
 **/
-//var displayThis = "Hey this should be a box with text in it";
+
+
+var displayThis = "Hey this should be a box with text in it";
+var generateDiv = {
+    view: function () {
+        m('div', displayThis)
+    }
+}
 
 const Button = {
     view(vnode) {
@@ -56,15 +59,16 @@ const Button = {
             m("button", {
                 onclick() {
                     console.log(vnode);
-                    var displayThis = "Hey this should be a box with text in it";
+                    m(generateDiv.view);
+                    displayThis = "This is J&H";
 
                 }
 
             }, "Button"),
-            m('div', displayThis)
+
         ];
     }
-    
+
 };
 
 m.mount(document.body, {
